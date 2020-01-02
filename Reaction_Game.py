1e474f5b947db19759a2467f01785abb8f99e245
Todd Pederson <34252100+Toddpe@users.noreply.github.com>
2020-01-02 05:25:03 +0000

    Update Reaction_Game.py

--- Reaction_Game.py
+++ Reaction_Game.py
@@ -1,14 +1,3 @@
-# Wire the signal LED to GPIO 17
-# Wire player 1's cheater LED to GPIO 22
-# Wire player 2's cheater LED to GPIO 27
-# Wire player 1's winning LED to GPIO 15
-# Wire player 2's winning LED to GPIO 14
-# Wire player 1's button to GPIO 4
-# Wilre player 2's a button to GPIO 18
-
-#                               OBJECT OF THE GAME:
-# The first player to press their buttion after the signal light goes off wins!
-# Be sure not to cheat.
 
 from gpiozero import Button, LED, PWMLED
 from time import sleep
@@ -22,7 +11,7 @@ player_2_winner = PWMLED(14)
 
 player_1 = Button(4)
 player_2 = Button(18)
-sec = 0
+num = 0
 cheater = False
 
 time = random.uniform(5, 10)
@@ -41,15 +30,14 @@ while True:
     if cheater == True:
         break
     if player_1.is_pressed:
-        print("Player 1 wins!")
+        print ("Player 1 wins in {} tenths of a second!".format(num))
         player_1_winner.pulse(2)
         break
     if player_2.is_pressed:
-        print("Player 2 wins!")
+        print (" Player 2 wins! in {} tenths of a second!".format(num))
         player_2_winner.pulse(2)
         break
     else:
-        print(sec)
-        sec = sec + 1
-        sleep(0.01)
+        num = num + 1
+        sleep(0.1)
 led.off()
